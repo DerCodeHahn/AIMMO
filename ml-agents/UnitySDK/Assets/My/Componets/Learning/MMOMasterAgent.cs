69cb9e75b0dbdca49c29d7054ac67998e083124f
DerHahn <christopher.hahn@exozet.com>
2019-05-31 17:48:43 +0200

    first step to master worker system

---
+++ ml-agents/UnitySDK/Assets/My/Componets/Learning/MMOMasterAgent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MLAgents;
+using UnityEngine;
+
+public class MMOMasterAgent : Agent
+{
+    enum BuildAction
+    {
+        none,
+        Worker,
+        House
+    }
+
+    public override void AgentReset()
+    {
+
+    }
+
+    public override void CollectObservations()
+    {
+
+    }
+    private void SetMask()
+    {
+
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+
+    }
+}
